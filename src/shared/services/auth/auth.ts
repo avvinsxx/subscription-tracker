7f57b7d4462164efe373854b82a7a000254ffc36
avvins@yandex.ru <-local>
2025-12-19 23:37:39 +0700

    Добавление BetterAuth для входа

---
+++ src/shared/services/auth/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins";
+
+import { prismaClient } from "@/shared/db/prisma/client";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prismaClient, {
+    provider: "postgresql",
+  }),
+  emailVerification: {
+    autoSignInAfterVerification: true,
+  },
+  plugins: [
+    emailOTP({
+      sendVerificationOnSignUp: true,
+      otpLength: 6,
+      expiresIn: 600,
+      async sendVerificationOTP({ email, otp, type }) {
+        console.log({ type });
+        if (type === "sign-in") {
+          // Send the OTP for sign in
+        } else if (type === "email-verification") {
+          // Send the OTP for email verification
+        } else {
+          // Send the OTP for password reset
+        }
+      },
+    }),
+  ],
+});
